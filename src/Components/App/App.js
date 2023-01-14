53b7f44a0827992d0532796a2fa0884473654a40
WilliamEm <william.emanuelsson@gmail.com>
2023-01-14 13:55:07 +0800

    comment html comments to prevent breaking

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,30 @@
-<div>
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+/* <div>
   <h1>Ja<span class="highlight">mmm</span>ing</h1>
   <div class="App">
     <!-- Add a SearchBar component -->
@@ -7,4 +33,4 @@
       <!-- Add a Playlist component -->
     </div>
   </div>
-</div>
+</div> */
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,4 +1,4 @@
-<div className="SearchResults">
+/* <div className="SearchResults">
   <h2>Results</h2>
   <!-- Add a TrackList component -->
-</div>
+</div> */
