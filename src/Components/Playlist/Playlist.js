c54ea7d8ea068005d4e28c04b191251fbb60764a
WilliamEm <william.emanuelsson@gmail.com>
2023-01-14 15:45:23 +0800

    add hardcoded search results and pass down state

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './Playlist.css';
 
-import {TrackList} from '../TrackList/TrackList';
+//import {TrackList} from '../TrackList/TrackList';
 
 export class Playlist extends React.Component {
     render() {
         return (
             <div class="Playlist">
-            <input value="New Playlist"/>
-            <TrackList />
+            <input defaultValue={'New Playlist'}/>
+            {/* <TrackList /> */}
             <button class="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
         );
