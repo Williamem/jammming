f098f9ecc8e0c7ad8e682b79fb24cc36981e3b3d
WilliamEm <william.emanuelsson@gmail.com>
2023-01-15 10:33:35 +0800

    add search logic (not connected to api)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,6 +13,7 @@ export class App extends React.Component {
         this.removeTrack = this.removeTrack.bind(this);
         this.updatePlaylistName = this.updatePlaylistName.bind(this);
         this.savePlaylist = this.savePlaylist.bind(this);
+        this.search = this.search.bind(this);
 
         this.state = {
             searchResults: [
@@ -52,12 +53,17 @@ export class App extends React.Component {
         return this.state.playlistTracks.map(track => track.uri);
     }
 
+    search(term) {
+        console.log(term);
+    }
+
     render() {
         return (
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                    <SearchBar />
+                    <SearchBar
+                    onSearch={this.search} />
                     <div className="App-playlist">
                         <SearchResults
                         searchResults={this.state.searchResults}
