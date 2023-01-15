45ade9c22c0cc877a3b1c6be4a68e64b48f8f1fa
WilliamEm <william.emanuelsson@gmail.com>
2023-01-15 08:17:11 +0800

    pass around and render playlist from App

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './Playlist.css';
 
-//import {TrackList} from '../TrackList/TrackList';
+import {TrackList} from '../TrackList/TrackList';
 
 export class Playlist extends React.Component {
     render() {
         return (
             <div class="Playlist">
             <input defaultValue={'New Playlist'}/>
-            {/* <TrackList /> */}
+            <TrackList
+            tracks={this.props.playlistTracks} />
             <button class="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
         );
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,7 +1,12 @@
 import React from "react";
 import './Track.css';
 
+    //remove below line later
+    let isRemoval;
+
 export class Track extends React.Component {
+
+
 
     renderAction() {
         if (isRemoval) {
@@ -15,10 +20,10 @@ export class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3>{/* <!-- track name will go here --> */}</h3>
-                    <p>{/* <!-- track artist will go here--> */} | {/* <!-- track album will go here --> */}</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
-                {this.renderAction}
+                {this.renderAction()}
             </div>
         );
     }
